880beb37417ab2a1e7f1936bd69980d9b41111ad
jurmaev <smartemii03@gmail.com>
2024-02-29 12:40:13 +0500

    fix: version command

--- src/cli/commands/version.command.ts
+++ src/cli/commands/version.command.ts
@@ -4,17 +4,12 @@ import { version } from '../../../package.json';
 export class VersionCommand implements Command {
   constructor(private readonly filePath: string = './package.json') {}
 
-  private readVersion(): string {
-    return version;
-  }
-
   public getName(): string {
     return '--version';
   }
 
   public async execute(..._parameters: string[]): Promise<void> {
     try {
-      const version = this.readVersion();
       console.info(version);
     } catch (error: unknown) {
       console.error(`Failed to read version from ${this.filePath}`);
