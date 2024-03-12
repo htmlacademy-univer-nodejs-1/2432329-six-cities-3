afd123f8304010d6d730b4ab1b2036f2e6995d31
jurmaev <smartemii03@gmail.com>
2024-03-12 21:05:51 +0500

    feat: add generator

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,3 +3,4 @@ export * from './command-parser.ts';
 export * from './commands/help.command.ts';
 export * from './commands/version.command.ts';
 export * from './commands/import.command.ts';
+export * from './commands/generate.command.ts';
