3f68c31f8c160ed799797e9a6f223b4d432dd738
jurmaev <smartemii03@gmail.com>
2024-03-19 21:41:02 +0500

    refactor: rest config

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,9 +1,29 @@
+import { config } from 'dotenv';
+import { Logger } from '../logger';
 import { Config } from './config.interface';
+import { RestSchema, configRestSchema } from './rest.schema';
 
-export class RestConfig implements Config {
-  constructor(private readonly config: NodeJS.ProcessEnv) {}
+export class RestConfig implements Config<RestSchema> {
+  private readonly config: RestSchema;
 
-  public get(key: string): string | undefined {
+  constructor(private readonly logger: Logger) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error(
+        'Can\'t read .env file. Perhaps the file does not exists.'
+      );
+    }
+
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+
+    this.config = configRestSchema.getProperties();
+
+    this.logger.info('.env file found and successfully parsed!');
+  }
+
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
     return this.config[key];
   }
 }
