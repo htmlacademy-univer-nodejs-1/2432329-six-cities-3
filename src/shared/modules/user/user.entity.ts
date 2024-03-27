a630366aecccce6736d3ff175177a60afbd9c14d
jurmaev <smartemii03@gmail.com>
2024-03-27 19:51:55 +0500

    feat: add user entity

---
+++ src/shared/modules/user/user.entity.ts
@@ -0,0 +1,21 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
+import { User, UserType } from '../../types';
+
+export class UserEntity implements User {
+  @prop({ required: true, minlength: 1, maxlength: 15 })
+  public name: string;
+
+  @prop({ unique: true, required: true })
+  public email: string;
+
+  @prop({ required: false })
+  public avatarUrl?: string;
+
+  @prop({ required: true, minlength: 6, maxlength: 12 })
+  public password: string;
+
+  @prop({ required: true })
+  public userType: UserType;
+}
+
+export const UserModel = getModelForClass(UserEntity);
