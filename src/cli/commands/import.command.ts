9b20ba6e60ff21a38e11807eb9a7d529f3c25f14
jurmaev <smartemii03@gmail.com>
2024-03-27 21:24:28 +0500

    refactor: import command

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,23 +1,96 @@
 import { Command } from './command.interface.js';
 import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
-import { createOffer, getErrorMessage } from '../../shared/helpers/index.js';
+import {
+  createOffer,
+  getErrorMessage,
+  getMongoURI,
+} from '../../shared/helpers/index.js';
+import { DatabaseClient } from '../../shared/libs/database-client/index.js';
+import { ConsoleLogger, Logger } from '../../shared/libs/logger/index.js';
+import { UserService } from '../../shared/modules/user/user-service.interface.js';
+import { OfferService } from '../../shared/modules/offer/offer-service.interface.js';
+import { CommentService } from '../../shared/modules/comment/comment-service.interface.js';
+import { DefaultUserService } from '../../shared/modules/user/default-user.service.js';
+import { UserModel } from '../../shared/modules/user/user.entity.js';
+import {
+  DefaultOfferService,
+  OfferModel,
+} from '../../shared/modules/offer/index.js';
+import {
+  CommentModel,
+  DefaultCommentService,
+} from '../../shared/modules/comment/index.js';
+import { Offer } from '../../shared/types/index.js';
+
+export const DEFAULT_DB_PORT = '27017';
 
 export class ImportCommand implements Command {
+  private databaseClient: DatabaseClient;
+  private logger: Logger;
+  private salt: string;
+  private userService: UserService;
+  private offerService: OfferService;
+  private commentService: CommentService;
+
+  constructor() {
+    this.onImportedLine = this.onImportedLine.bind(this);
+    this.onCompleteImport = this.onCompleteImport.bind(this);
+
+    this.logger = new ConsoleLogger();
+    this.userService = new DefaultUserService(this.logger, UserModel);
+    this.offerService = new DefaultOfferService(this.logger, OfferModel);
+    this.commentService = new DefaultCommentService(this.logger, CommentModel);
+  }
+
   public getName(): string {
     return '--import';
   }
 
-  private onImportedLine(line: string) {
+  private async onImportedLine(line: string, resolve: () => void) {
     const offer = createOffer(line);
-    console.info(offer);
+
+    await this.saveOffer(offer);
+    resolve();
+  }
+
+  private async saveOffer(offer: Offer) {
+    // const categories: string[] = [];
+    const user = await this.userService.findOrCreate(
+      {
+        ...offer.author,
+        password: '12345',
+      },
+      this.salt
+    );
+
+    // for (const { name } of offer.categories) {
+    //   const existCategory =
+    //     await this.categoryService.findByCategoryNameOrCreate(name, { name });
+    //   categories.push(existCategory.id);
+    // }
+
+    await this.offerService.create({
+      ...offer,
+      author: user,
+    });
   }
 
   private onCompleteImport(count: number) {
     console.info(`${count} rows imported.`);
+    this.databaseClient.disconnect();
   }
 
-  public async execute(...parameters: string[]): Promise<void> {
-    const [filename] = parameters;
+  public async execute(
+    filename: string,
+    host: string,
+    dbname: string,
+    salt: string
+  ): Promise<void> {
+    const uri = getMongoURI(host, DEFAULT_DB_PORT, dbname);
+    this.salt = salt;
+
+    await this.databaseClient.connect(uri);
+
     const fileReader = new TSVFileReader(filename.trim());
 
     fileReader.on('line', this.onImportedLine);
--- src/shared/modules/user/dto/create-user.dto.ts
+++ src/shared/modules/user/dto/create-user.dto.ts
@@ -3,7 +3,7 @@ import { UserType } from '../../../types';
 export class CreateUserDto {
   public name: string;
   public email: string;
-  public avatarUrl: string;
+  public avatarUrl?: string;
   public password: string;
   public userType: UserType;
 }
